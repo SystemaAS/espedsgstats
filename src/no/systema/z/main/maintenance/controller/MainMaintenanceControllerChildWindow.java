ca3567922c9f46b10c029dc6b697bcd8d6401444
oscardelatorre <oscardelatorre@localhost>
2021-12-13 23:33:35 +0100

    Update

--- src/no/systema/z/main/maintenance/controller/MainMaintenanceControllerChildWindow.java
+++ src/no/systema/z/main/maintenance/controller/MainMaintenanceControllerChildWindow.java
@@ -6,7 +6,7 @@ import java.io.FileOutputStream;
 import java.util.*;
 
 import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.*;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -75,7 +75,7 @@ import no.systema.z.main.maintenance.util.MainMaintenanceConstants;
 @Scope("session")
 public class MainMaintenanceControllerChildWindow {
 	
-	private static final Logger logger = Logger.getLogger(MainMaintenanceControllerChildWindow.class.getName());
+	private static final Logger logger = LogManager.getLogger(MainMaintenanceControllerChildWindow.class.getName());
 	private static final JsonDebugger jsonDebugger = new JsonDebugger(800);
 	//customer
 	
@@ -89,7 +89,7 @@ public class MainMaintenanceControllerChildWindow {
 	@PostConstruct
 	public void initIt() throws Exception {
 		if("DEBUG".equals(AppConstants.LOG4J_LOGGER_LEVEL)){
-			logger.setLevel(Level.DEBUG);
+			//logger.setLevel(Level.DEBUG);
 		}
 	}
 	
