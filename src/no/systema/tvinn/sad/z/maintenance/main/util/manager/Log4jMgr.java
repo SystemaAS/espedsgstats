fa4d91ecc488dd1c55d3ebf3ed7e3f7d325726f6
oscardelatorre <oscardelatorre@localhost>
2021-12-14 14:46:15 +0100

    Update

--- src/no/systema/tvinn/sad/z/maintenance/main/util/manager/Log4jMgr.java
+++ src/no/systema/tvinn/sad/z/maintenance/main/util/manager/Log4jMgr.java
@@ -8,11 +8,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Category;
-import org.apache.log4j.Level;
+
 import org.apache.logging.log4j.*;
-import org.apache.log4j.PropertyConfigurator;
+
 
 import no.systema.main.context.TdsServletContext;
 import no.systema.main.controller.GeneralTextRenderController;
@@ -75,6 +73,7 @@ public class Log4jMgr {
 	}
 
 	private void loadLog4jPropertiesFile() {
+		/*
 		String webAppPath = TdsServletContext.getTdsServletContext().getRealPath("/");
 		String log4jLocation = webAppPath + "WEB-INF/classes/log4j.properties";
 
@@ -93,6 +92,7 @@ public class Log4jMgr {
 				BasicConfigurator.configure();
 			}
 		}
+		*/
 	}
 	
 	public void doLogoutLogger(){
